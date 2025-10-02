29fe02df82514b0ac6e89cd40ac5ab750c85ff11
getill <theo.lemray@gmail.com>
2025-10-02 12:29:38 +0200

    Simulate pages relocated

---
+++ apps/web/app/pages/simulate/details.vue
@@ -0,0 +1,49 @@
+<script setup lang="ts">
+const data = ref([
+  {
+    Serveur: "4600",
+    CPU: "2024-03-11T15:30:00",
+    RAM: "paid",
+    Bandwith: "james.anderson@example.com",
+    Region: 594,
+    OS: Linux,
+    Type: VM,
+    Stockage: 1tb,
+    Prix Mensuel: 100€,
+    Prix horaire: 0.01€,
+    Type d instance: Partagé,
+  },
+  {
+    Serveur: "4600",
+    CPU: "2024-03-11T15:30:00",
+    RAM: "paid",
+    Bandwith: "james.anderson@example.com",
+    Region: 594,
+  },
+  {
+    Serveur: "4600",
+    CPU: "2024-03-11T15:30:00",
+    RAM: "paid",
+    Bandwith: "james.anderson@example.com",
+    Region: 594,
+  },
+  {
+    Serveur: "4600",
+    CPU: "2024-03-11T15:30:00",
+    RAM: "paid",
+    Bandwith: "james.anderson@example.com",
+    Region: 594,
+  },
+  {
+    Serveur: "4600",
+    CPU: "2024-03-11T15:30:00",
+    RAM: "paid",
+    Bandwith: "james.anderson@example.com",
+    Region: 594,
+  },
+]);
+</script>
+
+<template>
+  <UTable :data="data" class="flex-1" />
+</template>
