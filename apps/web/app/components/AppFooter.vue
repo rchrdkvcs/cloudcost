2f051f37e7b919efad83d7dabdf69d1436abcc8f
getill <theo.lemray@gmail.com>
2025-10-02 10:59:50 +0200

    Header and footer components

---
+++ apps/web/app/components/AppFooter.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts"></script>
+
+<template>
+  <UFooter>
+    <template #left>
+      <p class="text-muted text-sm">
+        Copyright © {{ new Date().getFullYear() }}
+      </p>
+    </template>
+
+    <template #right>
+      <UButton
+        icon="i-simple-icons-x"
+        color="neutral"
+        variant="ghost"
+        to="https://go.nuxt.com/x"
+        target="_blank"
+        aria-label="X"
+      />
+      <UButton
+        icon="i-simple-icons-github"
+        color="neutral"
+        variant="ghost"
+        to="https://github.com/rchrdkvcs"
+        target="_blank"
+        aria-label="GitHub"
+      />
+    </template>
+  </UFooter>
+</template>
+
+<style scoped></style>
---
+++ apps/web/app/components/AppHeader.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import type { NavigationMenuItem } from "@nuxt/ui";
+
+const headerItems = computed<NavigationMenuItem[]>(() => [
+  {
+    label: "Accueil",
+    icon: "lucide:home",
+    to: "/",
+  },
+  {
+    label: "Comparer",
+    icon: "lucide:compass",
+    to: "/simulate",
+  },
+]);
+</script>
+
+<template>
+  <UHeader>
+    <template #title>
+      <h1>CloudCost</h1>
+    </template>
+
+    <UNavigationMenu :items="headerItems" />
+  </UHeader>
+</template>
+
+<style scoped></style>
