5535b35e31c53e822018eb653454963f9fcc3bea
rchrdkvcs <gumimacihun@gmail.com>
2025-10-02 16:27:27 +0200

    fix: remove a random 2

--- apps/api/start/routes.ts
+++ apps/api/start/routes.ts
@@ -8,9 +8,7 @@
 */
 
 import router from '@adonisjs/core/services/router'
-2
-const GetPricingController = () => import('#prices/controllers/get_pricing_controller')
-const GetHealthController = () => import('#health/controllers/get_health_controller')
 
-router.get('/health', [GetHealthController, 'execute']).as('health')
+const GetPricingController = () => import('#prices/controllers/get_pricing_controller')
+
 router.get('/pricing', [GetPricingController, 'execute']).as('pricing')
